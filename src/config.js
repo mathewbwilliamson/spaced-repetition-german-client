a7ec357d065d0ce70303f9c284ddf48544d1e0e3
Mathew Williamson <mwilliamson@64labs.com>
2019-01-03 09:09:01 -0500

    Changed the server location internally

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-export const prodEndpoint = `https://spaced-repetition-api.herokuapp.com/`;
+export const prodEndpoint = `https://spaced-rep-german-server.herokuapp.com/`;
 export const endpoint = `http://localhost:4000/`;
